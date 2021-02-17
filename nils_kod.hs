8d7004cddf2815ae0807384437d395bc55fc6114
Nils Hartman <nils.hartman@rocketmail.com>
2021-02-17 11:20:29 +0100

    grundkoden

--- nils_kod.hs
+++ nils_kod.hs
@@ -1,1 +1,33 @@
+{-Pkd projekt Henrik Jädersten, Nils Hartman, Albin Bergkvist-}
+
+type Board = [(Grid,Square)]
+
+type Grid = (Int,Int)
+
+data Square = Empty | Piece Type Color
+
+data Type = K | Q | R | B | N | P
+
+data Color = Black | White
+
+
+newGame :: Board
+newGame = undefined
+
+
+move :: Grid -> Grid -> Board -> Board
+move = undefined
+
+
+availableSquare :: Square -> Bool
+availableSquare Empty = False
+availableSquare (Piece _ _) = True
+
+
+validMove :: Square -> Board -> [Grid]
+validMove = undefined
+
+
+victory :: Board -> Bool
+victory = undefined
 
